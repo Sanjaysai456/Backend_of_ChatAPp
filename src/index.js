705ac8ce33e47b2829e1c18df51bbfb4d3780bff
Sanjaysai456 <polojisanjaysai9849@gmail.com>
2025-02-26 14:55:28 +0530

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,33 @@
+import express from "express"
+import authRoutes from "./routes/auth.route.js"
+import messageRoutes from "./routes/message.route.js"
+import dotenv from "dotenv"
+import { connectDB } from "./lib/db.js";
+import cookieParser from "cookie-parser";
+import cors from "cors"
+import bodyParser from "body-parser";
+import {app , server} from './lib/socket.js'
+dotenv.config()
+
+
+
+app.use(cors({
+    origin:"http://localhost:5173",
+    credentials:true
+}))
+
+
+app.use(express.json())
+
+app.use(cookieParser());
+
+ 
+app.use("/auth", authRoutes);
+app.use("/messages", messageRoutes);
+
+
+server.listen(process.env.PORT, () => {
+    console.log("Server is running ");
+    connectDB()
+})
+
